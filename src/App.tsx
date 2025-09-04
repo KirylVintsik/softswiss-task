77cb2736bd4b57a4c8e23417897608ca185b83e5
Kiryl Vintsik <vintik.kirill@gmail.com>
2025-09-04 12:07:21 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Cart</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default App;
