f3b6ffacad05f67ab5478027d741fcdd55f85e8b
Kiryl Vintsik <vintik.kirill@gmail.com>
2025-09-04 17:27:48 +0200

    header nav

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [pluginReact(), pluginSass()],
   html: {
     title: 'SoftSwiss Task',
+    tags: [
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+        },
+      },
+    ],
   },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import './App.scss';
+import Logo from './assets/LOGO.svg';
+import { CartLogo } from './cart-logo.component';
 
 const App = () => {
   return (
-    <>
-      <header>
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Cart</a></li>
-          </ul>
+    <div className="app">
+      <header className="header">
+        <nav className="header_nav">
+          <div className="header_nav_content">
+            <a className="header_logo" href="#">
+              <img alt="Home" src={Logo} />
+            </a>
+            <ul className="header_menu">
+              <li>
+                <a className="nav_link" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="nav_link" href="#">
+                  Products
+                </a>
+              </li>
+              <a className="cart_logo" href="#">
+                <CartLogo className="cart_logo_svg" />
+              </a>
+            </ul>
+          </div>
         </nav>
+        <section className="header_banner"></section>
       </header>
-      <main></main>
-      <footer></footer>
-    </>
+      <main className="main"></main>
+      <footer className="footer"></footer>
+    </div>
   );
 };
 
